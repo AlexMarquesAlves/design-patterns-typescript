c61798d789581b36289584e636b7b6ba01cd45c8
¡Duque <atanaelleonardo@gmail.com>
2022-11-18 01:08:04 -0300

    Created main builder 'dish'

--- src/creational/builder/index.ts
+++ src/creational/builder/index.ts
@@ -1,12 +1,11 @@
-import { MealBox } from "./classes/meal-box";
-import { Beans, Beverage, Dessert, Meat, Rice } from "./classes/meals";
+import { log } from "console";
+import { MainDishBuilder } from "./classes/main-dish-builder";
 
-const rice = new Rice("Arroz", 5);
-const beans = new Beans("Feijão", 10);
-const meat = new Meat("Carne", 20);
-const beverage = new Beverage("Bebida", 15);
-const dessert = new Dessert("Sobremesa", 25);
-const mealBox = new MealBox();
-mealBox.add(rice, beans, meat, beverage, dessert);
-console.log(mealBox);
-console.log(mealBox.getPrice());
+const mainDishBuilder = new MainDishBuilder();
+mainDishBuilder.makeMeal();
+log(mainDishBuilder.getMeals());
+log(mainDishBuilder.getPrice());
+
+mainDishBuilder.reset();
+const meal2 = mainDishBuilder.makeBeverage().getMeals();
+log(meal2);
