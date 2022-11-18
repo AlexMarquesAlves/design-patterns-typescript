8ebde4e039e501d0c2298e2e3768cdfdd766a04f
¡Duque <atanaelleonardo@gmail.com>
2022-11-17 23:14:15 -0300

    created module a

---
+++ src/creational/singleton/module_a.ts
@@ -0,0 +1,8 @@
+import { MyDatabaseClassic } from "./db/my-database-classic";
+
+const myDatabaseClassic = MyDatabaseClassic.getInstance();
+myDatabaseClassic.add({ name: "Leo", age: 22 });
+myDatabaseClassic.add({ name: "Eliz", age: 38 });
+myDatabaseClassic.add({ name: "Luiz", age: 50 });
+myDatabaseClassic.remove(1);
+myDatabaseClassic.show();
