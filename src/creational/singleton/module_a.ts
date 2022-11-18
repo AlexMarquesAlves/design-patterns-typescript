4e6a4cfcaa589d9e74194ef8eb2ce1aab4ab212a
¡Duque <atanaelleonardo@gmail.com>
2022-11-17 23:46:49 -0300

    𝟛𝕣𝕕 𝔼𝕩𝕖𝕞𝕡𝕝𝕠 𝕕𝕖 𝕤𝕚𝕟𝕘𝕝𝕖𝕥𝕠𝕟

--- src/creational/singleton/module_a.ts
+++ src/creational/singleton/module_a.ts
@@ -1,6 +1,6 @@
-import { MyDatabaseClassic } from "./db/my-database-classic";
+import { MyDatabaseModule } from "./db/my-database-module";
 
-const myDatabaseClassic = MyDatabaseClassic.instance;
+const myDatabaseClassic = MyDatabaseModule;
 myDatabaseClassic.add({ name: "Leo", age: 22 });
 myDatabaseClassic.add({ name: "Eliz", age: 38 });
 myDatabaseClassic.add({ name: "Luiz", age: 50 });
--- src/creational/singleton/module_b.ts
+++ src/creational/singleton/module_b.ts
@@ -1,7 +1,7 @@
-import { MyDatabaseClassic } from "./db/my-database-classic";
+import { MyDatabaseModule } from "./db/my-database-module";
 import { myDatabaseClassic as myDatabaseClassicFromModuleA } from "./module_a";
 
-const myDatabaseClassic = MyDatabaseClassic.instance;
+const myDatabaseClassic = MyDatabaseModule;
 myDatabaseClassic.add({ name: "Roberto", age: 30 });
 myDatabaseClassic.add({ name: "Joanna", age: 50 });
 myDatabaseClassic.add({ name: "Luíza", age: 25 });
