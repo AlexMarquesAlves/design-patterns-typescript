2129dbf027149f1022a325e5f0987061d175034d
¡Duque <atanaelleonardo@gmail.com>
2022-11-18 01:17:30 -0300

    𝕔𝕣𝕖𝕒𝕥𝕖𝕕 𝕧𝕖𝕘𝕒𝕟 𝕕𝕚𝕤𝕙 𝕓𝕦𝕚𝕝𝕕𝕖𝕣

--- src/creational/builder/index.ts
+++ src/creational/builder/index.ts
@@ -1,11 +1,21 @@
 import { log } from "console";
 import { MainDishBuilder } from "./classes/main-dish-builder";
+import { VeganDishBuilder } from "./classes/vegan-dish-builder";
 
 const mainDishBuilder = new MainDishBuilder();
 mainDishBuilder.makeMeal();
 log(mainDishBuilder.getMeals());
 log(mainDishBuilder.getPrice());
 
+log("\n");
+
 mainDishBuilder.reset();
 const meal2 = mainDishBuilder.makeBeverage().getMeals();
 log(meal2);
+
+log("\n");
+
+const veganDishBuilder = new VeganDishBuilder();
+const veganMeal = veganDishBuilder.makeMeal().getMeals();
+log(veganMeal);
+log(veganMeal.getPrice());
