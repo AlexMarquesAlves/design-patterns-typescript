c00385b0ffb24b585db8c079ebe6f27f02181774
¡Duque <atanaelleonardo@gmail.com>
2022-12-02 00:23:25 -0300

    𝑪𝒐𝒎𝒎𝒂𝒏𝒅 - 𝑷𝒓á𝒕𝒊𝒄𝒂

--- src/behavioural/command/main.ts
+++ src/behavioural/command/main.ts
@@ -1,11 +1,11 @@
-import { LightPowerCommand } from './light-power-command';
-import { SmartHouseLight } from './smart-house-light';
-import { SmartHouseApp } from './smart-house-app';
-import { LightIntensityCommand } from './light-intensity-command';
+import { LightIntensityCommand } from "./light-intensity-command";
+import { LightPowerCommand } from "./light-power-command";
+import { SmartHouseApp } from "./smart-house-app";
+import { SmartHouseLight } from "./smart-house-light";
 
 // Receiver
-const bedroomLight = new SmartHouseLight('Luz Quarto');
-const bathroomLight = new SmartHouseLight('Luz Banheiro');
+const bedroomLight = new SmartHouseLight("Luz Quarto");
+const bathroomLight = new SmartHouseLight("Luz Banheiro");
 
 // Command
 const bedroomLightPowerCommand = new LightPowerCommand(bedroomLight);
@@ -14,20 +14,20 @@ const bedroomIntensityCommand = new LightIntensityCommand(bedroomLight);
 
 // Controle - Invoker
 const smartHouseApp = new SmartHouseApp();
-smartHouseApp.addCommand('btn-1', bedroomLightPowerCommand);
-smartHouseApp.addCommand('btn-2', bathroomLightPowerCommand);
-smartHouseApp.addCommand('btn-3', bedroomIntensityCommand);
+smartHouseApp.addCommand("btn-1", bedroomLightPowerCommand);
+smartHouseApp.addCommand("btn-2", bathroomLightPowerCommand);
+smartHouseApp.addCommand("btn-3", bedroomIntensityCommand);
 
-smartHouseApp.executeCommand('btn-1');
-smartHouseApp.undoCommand('btn-1');
+smartHouseApp.executeCommand("btn-1");
+smartHouseApp.undoCommand("btn-1");
 
-smartHouseApp.executeCommand('btn-2');
-smartHouseApp.undoCommand('btn-2');
+smartHouseApp.executeCommand("btn-2");
+smartHouseApp.undoCommand("btn-2");
 
 for (let i = 0; i < 5; i++) {
-  smartHouseApp.executeCommand('btn-3');
+  smartHouseApp.executeCommand("btn-3");
 }
 
 for (let i = 0; i < 3; i++) {
-  smartHouseApp.undoCommand('btn-3');
+  smartHouseApp.undoCommand("btn-3");
 }
